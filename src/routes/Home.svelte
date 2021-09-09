0fa698a3fe7b2b1814d00d36990c94b8d4af6d72
Raafat Turki <raafat.turki@protonmail.com>
2021-09-09 20:45:47 +0300

    added image filtering

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,56 +1,84 @@
 <script lang="ts">
 	let previewed_image = 0
 	let is_previewed_image_visible = false
+	let update_groups_filter = 0
+	let groups = []
+	let selected_groups = []
 
 	let images = [
-		["eateot.jpg",			1,	1],
-		["fortvivlan.jpg",		2,	1],
-		["dimensia.jpg",		1,	1],
-		["fortvivlan.jpg",		2,	1],
-		["eateot.jpg",			1,	1],
-		["dimensia.jpg",		1,	1],
-		["fortvivlan.jpg",		2,	1],
-		["dimensia.jpg",		1,	1],
-		["dimensia.jpg",		1,	2],
-		["eateot.jpg",			1,	1],
-		["dimensia.jpg",		1,	1],
-		["eateot.jpg",			1,	1],
-		["eateot.jpg",			1,	1],
-		["fortvivlan.jpg",		2,	1],
-		["dimensia.jpg",		1,	1],
-		["eateot.jpg",			1,	1],
-		["dimensia.jpg",		1,	1],
-		["eateot.jpg",			1,	1],
-		["eateot.jpg",			1,	1],
-		["fortvivlan.jpg",		2,	1],
-		["dimensia.jpg",		1,	1],
-		["eateot.jpg",			1,	1],
-		["dimensia.jpg",		1,	1],
-		["eateot.jpg",			1,	1],
-		["eateot.jpg",			1,	1],
-		["fortvivlan.jpg",		2,	1],
-		["dimensia.jpg",		1,	1],
-		["dimensia.jpg",		1,	1],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
+		["eateot.jpg",			1,	1, ['eateot']],
+		["fortvivlan.jpg",		2,	1, ['fortvivlan']],
+		["dimensia.jpg",		1,	1, ['dimensia']],
 	]
+
+	for (let i = 0; i < images.length; i++) {
+		let curr_group_set = images[i][3]
+		// console.log(curr_group_set)
+		for (let j = 0; j < curr_group_set.length; j++) {
+			let curr_group = curr_group_set[j]
+			if (!groups.includes(curr_group)) groups.push(curr_group)
+		}
+	}
+
+	selected_groups = groups
 
 	function preview_image(n) {
 		previewed_image = n
 		is_previewed_image_visible = !is_previewed_image_visible
 	}
+
+	function is_image_passing_filter(image) {
+		let curr_group_set = image[3]
+		for (let i = 0; i < curr_group_set.length; i++) {
+			let curr_group = curr_group_set[i]
+			if (selected_groups.includes(curr_group)) return true
+		}
+		return false
+	}
 </script>
 
+<select class="filter_options" multiple bind:value={selected_groups} on:click={() => update_groups_filter++}>
+	{#each groups as group }
+		<option value={group}>{group}</option>
+	{/each}
+</select>
 
 <div class="gallery">
-	{#each images as image, i}	
-		<div
-			style="
-				background-image: url('./assets/{image[0]}');
-				grid-row: span {image[1]} / auto;
-				grid-column: span {image[2]} / auto;
-			"
-			on:click={() => {preview_image(i)}}
-		></div>
-	{/each}
+	{#key update_groups_filter}	
+		{#each images as image, i}
+		{#if is_image_passing_filter(image)}
+			<div
+				style="
+					background-image: url('./assets/{image[0]}');
+					grid-row: span {image[1]} / auto;
+					grid-column: span {image[2]} / auto;
+				"
+				on:click={() => {preview_image(i)}}
+			></div>
+		{/if}
+		{/each}
+	{/key}
 </div>
 
 <div
@@ -63,6 +91,22 @@
 </div>
 
 <style lang="scss">
+
+	.filter_options {
+		background: #0000;
+		border: none;
+
+		option {
+			color: white;
+			background: #0000;
+		}
+
+		option:hover {
+			color: black;
+			background: #FFF;
+		}
+	}
+
 	:global(body) {
 		background: black;
 		padding: 2rem;
